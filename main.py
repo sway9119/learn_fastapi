b2b6649c0fa2170c4997914c496f124e9779e6c2
sway <40759948+sway9119@users.noreply.github.com>
2022-11-23 17:19:54 +0900

    クエリパラメータの説明

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,3 +12,13 @@ app = FastAPI()
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+# クエリパラメータ
+# curl "http://127.0.0.1:8000/items/?skip=0&limit=10"
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
