9d00e7fe6c7e9bb15ab97105ef799c32cc34f8ad
sway <40759948+sway9119@users.noreply.github.com>
2023-01-05 23:32:28 +0900

    モデルの使用

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import Union
+from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -16,9 +18,21 @@ async def read_file(file_path: str):
 
 # クエリパラメータ
 # curl "http://127.0.0.1:8000/items/?skip=0&limit=10"
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
+
+# モデルの使用
+# curl "http://127.0.0.1:8000/items/?name=foo&price=4.2"
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
