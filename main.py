87682ec324305062f4cfefc9766c4286147ed385
sway <40759948+sway9119@users.noreply.github.com>
2023-01-07 16:42:26 +0900

    メタデータの追加

--- main.py
+++ main.py
@@ -48,3 +48,18 @@ async def read_items(
         results.update({"q": q})
     return results
 
+# より多くのメタデータを宣言する
+# curl -X 'GET' 'http://127.0.0.1:8000/items/' -H 'accept: application/json'
+@app.get("/items/")
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
